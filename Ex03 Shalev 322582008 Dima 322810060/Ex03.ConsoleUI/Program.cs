54558fd92f84cb2e1028f2b031c1c8be3b325fae
Dima252 <todoseyev.danny@gmail.com>
2025-05-27 19:54:49 +0300

    nothis is working

---
+++ Ex03 Shalev 322582008 Dima 322810060/Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    class Program
+    {
+        static void Main()
+        {
+            UIManager uiManager = new UIManager();
+            uiManager.Run();
+
+        }
+    }
+}
